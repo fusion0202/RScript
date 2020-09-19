d99f5a572e7eaa5579baa8cb48a6aab9b52ac45e
fusion0202 <sub.master.0202@gmail.com>
2020-09-19 17:13:56 +0900

    Update covid19_chiba_daily.R

--- covid19_chiba_daily.R
+++ covid19_chiba_daily.R
@@ -12,20 +12,21 @@ dts <- stl(ts(rep, frequency = 7), s.window = 'per')
 df <- data.frame(day, rep, trd = dts$time.series[, 2])
 
 mtitle <- paste0('Chiba, daily from ', df$day[1], ' to ', df$day[nrow(df)])
-datebreaks <- c(seq(as.Date("2020-02-01"), by = "month", length.out = 7),
-                seq(as.Date("2020-02-15"), by = "month", length.out = 7))
+datebreaks <- c(seq(as.Date("2020-02-01"), by = "month", length.out = 8),
+                seq(as.Date("2020-02-15"), by = "month", length.out = 8))
 
 g <- ggplot(data = df)
 g <- g + geom_segment(aes(x = day, y = 0, xend = day, yend=rep),
                       color = "lightblue", size = 1.5)
-g <- g + geom_line(aes(x = day, y = trd), color = "darkorange", size = 1.0, alpha= 0.8)
-g <- g + theme_minimal()
+g <- g + geom_line(aes(x = day, y = trd), color = "darkorange", size = 1.0, alpha　= 0.8)
+g <- g + theme_light()
 g <- g + scale_x_date(breaks = datebreaks, labels = date_format("%m/%d")) 
 g <- g + labs(title = mtitle,
               x = "Day", 
               y = "Positive reported",
               caption = paste("Data Source: ", url)) 
-g <- g + theme(plot.title = element_text(size = rel(1.4)),
+g <- g + theme(panel.grid.minor = element_blank(),
+               plot.title = element_text(size = rel(1.4)),
                axis.title = element_text(size = rel(1.2)),
                axis.text = element_text(size = rel(1.0)))
 print(g)
