8808c101d4ab2d802517b9d1a89f1ff9bb560a22
fusion0202 <sub.master.0202@gmail.com>
2020-11-07 05:25:49 +0900

    Update covid19_chiba_daily.R

--- covid19_chiba_daily.R
+++ covid19_chiba_daily.R
@@ -12,8 +12,8 @@ dts <- stl(ts(rep, frequency = 7), s.window = 'per')
 df <- data.frame(day, rep, trd = dts$time.series[, 2])
 
 mtitle <- paste0('Chiba, daily from ', df$day[1], ' to ', df$day[nrow(df)])
-datebreaks <- c(seq(as.Date("2020-02-01"), by = "month", length.out = 8),
-                seq(as.Date("2020-02-15"), by = "month", length.out = 8))
+datebreaks <- c(seq(as.Date("2020-02-01"), by = "month", length.out = 10),
+                seq(as.Date("2020-02-15"), by = "month", length.out = 9))
 
 g <- ggplot(data = df)
 g <- g + geom_segment(aes(x = day, y = 0, xend = day, yend=rep),
