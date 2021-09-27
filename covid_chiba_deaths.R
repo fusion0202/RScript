b46fefb1ee3a23292c05f8d94ceea38e0e70195b
fusion0202 <sub.master.0202@gmail.com>
2021-09-27 11:10:14 +0900

    Update covid_chiba_deaths.R

--- covid_chiba_deaths.R
+++ covid_chiba_deaths.R
@@ -21,14 +21,13 @@ df %>%
 url <- "https://github.com/fusion0202/RScript/blob/master/covid_chiba_deaths.csv"
 mtitle <- paste0('Daily New Deaths in Chiba, from ',
                  df2$date[1], ' to ', df2$date[length(df2$date)])
-datebreaks <- c(seq(as.Date("2020-02-01"), by = "month", length.out = 24))
+datebreaks <- c(seq(as.Date("2020-03-01"), by = "month", length.out = 24))
 
 
 g <- ggplot(data = df2)
 g <- g + geom_segment(aes(x = date, y = 0, xend = date, yend = Freq),
                       color = "blue", size = 0.5, alpha = 0.5)
 g <- g + theme_light()
-g <- g + ylim(0, 15)
 g <- g + scale_x_date(breaks = datebreaks, labels = date_format("%m/%d")) 
 g <- g + labs(title = mtitle,
               x = "Date", 
