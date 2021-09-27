dd0927d2ea1da88fdbd28c12cc90e6e674a59fcb
fusion0202 <sub.master.0202@gmail.com>
2021-09-27 10:50:49 +0900

    Update covid_chiba_deaths.R

--- covid_chiba_deaths.R
+++ covid_chiba_deaths.R
@@ -5,7 +5,6 @@ library(dplyr)
 library(tidyverse)
 
 URL <- "https://raw.githubusercontent.com/fusion0202/RScript/master/covid_chiba_deaths.csv"
-#URL <- "/home/my/data/Chiba_pref_covid/covid_chiba_deaths.csv"
 d <- read.csv(URL)
 
 
@@ -25,9 +24,9 @@ datebreaks <- c(seq(as.Date("2020-02-01"), by = "month", length.out = 24))
 
 g <- ggplot(data = df2)
 g <- g + geom_segment(aes(x = date, y = 0, xend = date, yend = Freq),
-                      color = "blue", size = 0.5, alpha=0.5)
+                      color = "blue", size = 0.5, alpha = 0.5)
 g <- g + theme_light()
-g <- g + ylim(0,15)
+g <- g + ylim(0, 15)
 g <- g + scale_x_date(breaks = datebreaks, labels = date_format("%m/%d")) 
 g <- g + labs(title = mtitle,
               x = "Date", 
